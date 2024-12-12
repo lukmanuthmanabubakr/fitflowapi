8e623ebbf493d99e35eaf8725ff02be2ec7e42c7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-12-12 11:08:12 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    img: {
+      type: String,
+      default: null,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    age: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
